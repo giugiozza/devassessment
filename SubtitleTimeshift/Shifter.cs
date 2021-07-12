7922ee9d241e5fc830a0328b4932f2e1ceab1f8d
Giulia Giozza <giuliagiozza@gmail.com>
2021-07-12 20:23:31 -0300

    functions' first draft

--- SubtitleTimeshift/Shifter.cs
+++ SubtitleTimeshift/Shifter.cs
@@ -9,7 +9,39 @@ namespace SubtitleTimeshift
     {
         async static public Task Shift(Stream input, Stream output, TimeSpan timeSpan, Encoding encoding, int bufferSize = 1024, bool leaveOpen = false)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var reader = new StreamReader(input, encoding, false, bufferSize, leaveOpen);
+                var writer = new StreamWriter(output, encoding, bufferSize, leaveOpen);
+
+                var line = await reader.ReadLineAsync();
+
+                while (line != null)
+                {
+                    if(isTheTimeLine(line))
+                    {
+                        updateTime(line, timeSpan);
+                    }
+                    else
+                    {
+                        writer.WriteLine(line);
+                    }
+                    line = await reader.ReadLineAsync();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed: {0}", e.ToString());
+            }
+        }
+
+        private static bool isTheTimeLine(string line)
+        {
+            return false;
+        }
+        private static bool updateTime(string line, TimeSpan timeSpan)
+        {
+            return true;
         }
     }
 }
