47d6d39d042a93b8272753a179821237a92d4b1d
Giulia Giozza <giuliagiozza@gmail.com>
2021-07-13 00:09:30 -0300

    calculating timespan and writting new file

--- SubtitleTimeshift/Shifter.cs
+++ SubtitleTimeshift/Shifter.cs
@@ -7,33 +7,37 @@ using System.Text.RegularExpressions;
 
 namespace SubtitleTimeshift
 {
+    static class Constants
+    {
+        public const string timePattern = @"(\d+)[:,](\d+)[:,](\d+)[,.](\d+)(\b --> \b)(\d+)[:,](\d+)[:,](\d+)[,.](\d+)";
+    }
     public class Shifter
     {
         async static public Task Shift(Stream input, Stream output, TimeSpan timeSpan, Encoding encoding, int bufferSize = 1024, bool leaveOpen = false)
         {
             try
             {
-                var reader = new StreamReader(input, encoding, false, bufferSize, leaveOpen);
-                var writer = new StreamWriter(output, encoding, bufferSize, leaveOpen);
+                using(StreamReader reader = new StreamReader(input)){
+                    using(StreamWriter writer = new StreamWriter(output)){
 
-                var line = await reader.ReadLineAsync();
-
-                while (line != null)
-                {
-                    if(isTheTimeLine(line))
-                    {
-                        updateTime(line, timeSpan);
+                        string line = await reader.ReadLineAsync();
+                        
+                        while (line != null)
+                        {
+                            if(isTheTimeLine(line))
+                            {
+                                line = updateTime(line, timeSpan);
+                            }
+                            writer.WriteLine(line);
+                            line = await reader.ReadLineAsync();
+                        }
                     }
-                    else
-                    {
-                        writer.WriteLine(line);
-                    }
-                    line = await reader.ReadLineAsync();
-                }
+                }               
             }
             catch (Exception e)
             {
-                Console.WriteLine("The process failed: {0}", e.ToString());
+                Console.WriteLine("The process failed.");
+                throw e;
             }
         }
 
@@ -42,16 +46,27 @@ namespace SubtitleTimeshift
         /// </summary>
         private static bool isTheTimeLine(string line)
         {
-            string timePattern = @"(\d+)[:,](\d+)[:,](\d+)[:,.](\d+)\b --> \b(\d+)[:,](\d+)[:,](\d+)[:,.](\d+)";
-            return Regex.IsMatch(line, timePattern);
+            return Regex.IsMatch(line, Constants.timePattern);
         }
 
         /// <summary>
         /// Updates the time with the time span.
         /// </summary>
-        private static bool updateTime(string line, TimeSpan timeSpan)
-        {
-            return true;
+        private static string updateTime(string line, TimeSpan timeSpan)
+        {            
+            var newLine = "";
+            Regex pattern = new Regex(Constants.timePattern);
+            Match match = pattern.Match(line);
+            if (match.Success)
+            {
+                newLine = new TimeSpan(0, int.Parse(match.Groups[1].Value), int.Parse(match.Groups[2].Value), int.Parse(match.Groups[3].Value),
+                    int.Parse(match.Groups[4].Value)).Add(timeSpan).ToString(@"hh\:mm\:ss\.fff")
+                    + match.Groups[5].Value
+                    + new TimeSpan(0, int.Parse(match.Groups[6].Value), int.Parse(match.Groups[7].Value), int.Parse(match.Groups[8].Value),
+                    int.Parse(match.Groups[9].Value)).Add(timeSpan).ToString(@"hh\:mm\:ss\.fff");
+            }
+
+            return newLine;
         }
     }
 }
